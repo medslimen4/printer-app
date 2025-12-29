20eac3e9c90f1492d6e251ba2f8676fec2511211
medsliman4 <medsliman4@gmail.com>
2025-12-29 09:05:02 +0000

    feat: Add logging to Bluetooth scan

--- lib/bluetooth_service.dart
+++ lib/bluetooth_service.dart
@@ -1,5 +1,6 @@
 
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -7,8 +8,21 @@ class AppBluetoothService {
   Stream<List<ScanResult>> get scanResults => FlutterBluePlus.scanResults;
 
   Future<void> startScan() async {
+    if (kDebugMode) {
+      print("Starting Bluetooth scan...");
+    }
     await _requestPermissions();
     await FlutterBluePlus.startScan(timeout: const Duration(seconds: 4));
+    scanResults.listen((results) {
+      if (kDebugMode) {
+        print("Found ${results.length} devices");
+      }
+      for (ScanResult r in results) {
+        if (kDebugMode) {
+          print('${r.device.name} found! rssi: ${r.rssi}');
+        }
+      }
+    });
   }
 
   Future<void> stopScan() async {
